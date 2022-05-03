a201224b3db05ac9754f29c64714eb2e297c9e31
Inspireed <vmeshkov312@gmail.com>
2022-05-03 23:07:09 +0300

    Deleted main

--- Tanki-Game/main.cpp
+++ Tanki-Game/main.cpp
@@ -3,7 +3,9 @@
 
 
 int main(int argc, char* argv[])
-{
+{ 
+    // TODO: handle startup options
+
 	Game game;
 	
 	game.run();
@@ -11,26 +13,4 @@ int main(int argc, char* argv[])
 	return 0;
 }
 
-//int main(int argc, char* argv[])
-//{
-//    // TODO: handle startup options
-//
-//    sf::RenderWindow window(sf::VideoMode(1280, 720), "Tanki!", sf::Style::Titlebar | sf::Style::Close);
-//    
-//    sf::CircleShape shape(100.f);
-//    shape.setFillColor(sf::Color::Yellow);
-//
-//    while (window.isOpen())
-//    {
-//        
-//
-//        window.clear();
-//        
-//        window.draw(shape);
-//        // TODO: Game
-//        
-//        window.display();
-//    }
-//
-//    return 0;
-//}
+
